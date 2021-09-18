712d2b6d76b826ed4698ccd4a7280801261cce47
Daniel Backes <danielbackes@protonmail.com>
2021-09-18 11:36:20 -0400

    feat: create integration test to the emails

--- tests/Service/MailerTest.php
+++ tests/Service/MailerTest.php
@@ -2,16 +2,18 @@
 
 namespace App\Tests\Service;
 
+use App\Entity\Article;
 use App\Entity\User;
 use App\Service\Mailer;
 use Knp\Snappy\Pdf;
 use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\NamedAddress;
 use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupInterface;
 use Twig\Environment;
 
-class MailerTest extends TestCase
+class MailerTest extends KernelTestCase
 {
     public function testSendWelcomeMessage()
     {
@@ -37,5 +39,28 @@ class MailerTest extends TestCase
         $this->assertInstanceOf(NamedAddress::class, $namedAddresses);
         $this->assertSame('Victor', $namedAddresses->getName());
         $this->assertSame('victor@symfonycasts.com', $namedAddresses->getAddress());
+    }
+
+    public function testIntegrationSendAuthorWeeklyReportMessage()
+    {
+        self::bootKernel();
+
+        $symfonyMailer = $this->createMock(MailerInterface::class);
+        $symfonyMailer->expects($this->once())
+            ->method('send');
+
+        $pdf = self::$container->get(Pdf::class);
+        $twig = self::$container->get(Environment::class);
+        $entrypointLookup = $this->createMock(EntrypointLookupInterface::class);
+
+        $user = new User();
+        $user->setFirstName('Victor');
+        $user->setEmail('victor@symfonycasts.com');
+        $article = new Article();
+        $article->setTitle('Black Holes: Ultimate Party Pooper');
+
+        $mailer = new Mailer($symfonyMailer, $twig, $pdf, $entrypointLookup);
+        $email = $mailer->sendAuthorWeeklyReportMessage($user, [$article]);
+        $this->assertCount(1, $email->getAttachments());
     }
 }
