ed6557e1e01f9d9f0b8bd664297f2b558dea5bb9
Daniel Backes <danielbackes@protonmail.com>
2021-09-18 10:53:30 -0400

    refactor: isolate the mailer logics in a specific service

--- src/Command/AuthorWeeklyReportSendCommand.php
+++ src/Command/AuthorWeeklyReportSendCommand.php
@@ -4,6 +4,7 @@ namespace App\Command;
 
 use App\Repository\ArticleRepository;
 use App\Repository\UserRepository;
+use App\Service\Mailer;
 use Knp\Snappy\Pdf;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Console\Command\Command;
@@ -24,20 +25,14 @@ class AuthorWeeklyReportSendCommand extends Command
     private $userRepository;
     private $articleRepository;
     private $mailer;
-    private $twig;
-    private $pdf;
-    private $entrypointLookup;
     
-    public function __construct(UserRepository $userRepository, ArticleRepository $articleRepository, MailerInterface $mailer, Environment $twig, Pdf $pdf, EntrypointLookupInterface $entrypointLookup)
+    public function __construct(UserRepository $userRepository, ArticleRepository $articleRepository, Mailer $mailer)
     {
         parent::__construct(null);
 
         $this->userRepository = $userRepository;
         $this->articleRepository = $articleRepository;
         $this->mailer = $mailer;
-        $this->twig = $twig;
-        $this->pdf = $pdf;
-        $this->entrypointLookup = $entrypointLookup;
     }
 
     protected function configure()
@@ -67,25 +62,7 @@ class AuthorWeeklyReportSendCommand extends Command
                 continue;
             }
 
-            $this->entrypointLookup->reset();
-            $html = $this->twig->render('email/author-weekly-report-pdf.html.twig', [
-                'articles' => $articles,
-            ]);
-
-            $pdf = $this->pdf->getOutputFromHtml($html);
-
-            $email = (new TemplatedEmail())
-                ->from(new NamedAddress('alienmailcarrier@example.com', 'The Space Bar'))
-                ->to(new NamedAddress($author->getEmail(), $author->getFirstName()))
-                ->subject('Your weekly report on the Space Bar!')
-                ->htmlTemplate('email/author-weekly-report.html.twig')
-                ->context([
-                    'author' => $author,
-                    'articles' => $articles,
-                ])
-                ->attach($pdf, sprintf('weekly-report-%s.pdf', date('Y-m-d')));
-
-            $this->mailer->send($email);
+            $this->mailer->sendAuthorWeeklyReportMessage($author, $articles);        
         }
 
         $io->progressFinish();
